d37b2ab279a331086c60d73a222129b2b65bebf1
yassowgh <yassow@gmail.com>
2026-07-10 08:51:51 +0100

    fix: use next/dynamic to keep page.tsx as true server component

--- src/app/quiz/[id]/edit/page.tsx
+++ src/app/quiz/[id]/edit/page.tsx
@@ -1,4 +1,5 @@
-import EditClient from './EditClient';
+import dynamic from 'next/dynamic';
+const EditClient = dynamic(() => import('./EditClient'), { ssr: false });
 
 export function generateStaticParams() { return []; }
 
